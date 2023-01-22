4c9de36f031b84b618cd0f473f2a87e5629212d2
Enzo Corte <122570330+enzoocorte@users.noreply.github.com>
2023-01-22 11:55:40 -0300

    Ocultando boton de ver contacto una vez presionado

--- Javascript/CV.js
+++ Javascript/CV.js
@@ -1,3 +1,4 @@
 document.getElementById("mostrar-contacto").addEventListener("click", function(){
     document.getElementById("contacto").style.display = "block";
+    document.getElementById("mostrar-contacto").style.display = "none";
   });
